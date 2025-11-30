5e791493a2f511e9fb11e2b95751a83be8590ffa
SyabAhmad <syabblogger@gmail.com>
2025-11-30 14:36:09 +0500

    Add merge migration to unify multiple heads and remove accidental root merges

---
+++ backend/migrations/versions/97026d9fb2ef_final_merge.py
@@ -0,0 +1,24 @@
+"""final_merge
+
+Revision ID: 97026d9fb2ef
+Revises: 
+Create Date: 2025-11-30 14:23:31.802641
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '97026d9fb2ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
