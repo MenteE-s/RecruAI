456316cd0c491b15504ecf126e34b0bb23a3a9e2
SyabAhmad <syabblogger@gmail.com>
2025-12-04 12:16:41 +0500

    Change school column to be nullable in Education model

--- backend/models/education.py
+++ backend/models/education.py
@@ -9,7 +9,7 @@ class Education(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     degree = db.Column(db.String(255), nullable=False)
-    school = db.Column(db.String(255), nullable=False)
+    school = db.Column(db.String(255), nullable=True)
     field = db.Column(db.String(255), nullable=True)
     year = db.Column(db.String(50), nullable=True)
     gpa = db.Column(db.String(10), nullable=True)
