0d9f017487e0bb70a22cbea65da127a96aa4ee23
syabahmad <syabblogger@gmail.com>
2026-09-08 09:23:07 +0300

    Restore CustomerSegments section to landing page

--- frontend/src/pages/RecruAILanding.js
+++ frontend/src/pages/RecruAILanding.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import HowItWorks from '../components/HowItWorks';
+import CustomerSegments from '../components/CustomerSegments';
 import FeaturedJobs from '../components/FeaturedJobs';
 import Pricing from '../components/Pricing';
 import Testimonials from '../components/Testimonials';
@@ -16,6 +17,7 @@ const RecruAILanding = () => {
       <Hero />
       <Features />
       <HowItWorks />
+      <CustomerSegments />
       <FeaturedJobs />
       <Pricing />
       <Testimonials />
