152692c7a8f9cd464605110681c3faf9099fc8ce
syabahmad <syabblogger@gmail.com>
2026-09-08 09:19:24 +0300

    Add smooth scrolling and remove CustomerSegments section

--- frontend/src/pages/RecruAILanding.js
+++ frontend/src/pages/RecruAILanding.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import HowItWorks from '../components/HowItWorks';
-import CustomerSegments from '../components/CustomerSegments';
 import FeaturedJobs from '../components/FeaturedJobs';
 import Pricing from '../components/Pricing';
 import Testimonials from '../components/Testimonials';
@@ -17,7 +16,6 @@ const RecruAILanding = () => {
       <Hero />
       <Features />
       <HowItWorks />
-      <CustomerSegments />
       <FeaturedJobs />
       <Pricing />
       <Testimonials />
